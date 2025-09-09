189438d2557014374614dfb14e78872992c2402b
Mateus Lago <mateusevaristolago@gmail.com>
2025-09-09 12:46:25 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using primercrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+ 
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Produtos}/{action=Index}/{id?}");
+
+app.Run();
