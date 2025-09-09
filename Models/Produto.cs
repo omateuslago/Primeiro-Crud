782c90e491564685200cf056a54ca3b9a442cb30
Mateus Lago <mateusevaristolago@gmail.com>
2025-09-09 12:44:16 -0300

    Create Produto.cs

---
+++ Models/Produto.cs
@@ -0,0 +1,10 @@
+namespace primercrud.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preço { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
