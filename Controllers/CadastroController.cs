1f9180f3230acece989ab8b33874e2e2b72fc394
Mateus Lago <mateusevaristolago@gmail.com>
2025-09-11 12:46:01 -0300

    Update CadastroController.cs

--- Controllers/CadastroController.cs
+++ Controllers/CadastroController.cs
@@ -5,18 +5,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace cruudd.Controllers
 {
+    // CadastroController: Classe que controla as rotas ligadas ao cadastro.
+    // : Controller: Classe "pai" do ASP.NET Core que oferece recursos para trabalhar com Views, HTTP e redirecionamentos.
     public class CadastroController : Controller
     {
+        // AppDbContext: classe que representa o banco de dados.
+        // _context: variável que vai armazenar a conexão com o banco e permitir manipular os dados.
         private readonly AppDbContext _context;
 
+
+        // Construtor do Controller: recebe o AppDbContext pronto (injeção de dependência).
+        // "context": variável local que chega pronta do ASP.NET com o banco configurado.
         public CadastroController(AppDbContext context)
         {
+            // Guarda o AppDbContext recebido no campo privado _context.
             _context = context;
         }
 
+        // Action Index: retorna a lista de cadastros.
+        // IActionResult: tipo de retorno que pode ser uma View, JSON, erro, etc.
         public async Task<IActionResult> Index()
         {
+            // _context.Cadastros: representa a tabela "Cadastros" no banco.
+            // ToListAsync(): busca todos os registros e transforma em uma lista.
             var cadastros = await _context.Cadastros.ToListAsync();
+
+            // Retorna a View Index.cshtml passando a lista (cadastros) como modelo.
             return View(cadastros);
         }
         public IActionResult Create()
