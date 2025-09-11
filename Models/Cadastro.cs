65a2228b167aa9a12ba805feb85b9fc2c4561f33
Mateus Lago <mateusevaristolago@gmail.com>
2025-09-11 10:42:40 -0300

    Update and rename Produto.cs to Cadastro.cs

---
+++ Models/Cadastro.cs
@@ -0,0 +1,10 @@
+namespace cruudd.Models
+{
+    public class Cadastro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public int Semestre { get; set; }
+    }
+}
